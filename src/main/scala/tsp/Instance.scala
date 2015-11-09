5e15f9982eeca5ceee6b7824ddd46ea982bb06d4
qw0te <baert.quentin@gmail.com>
2015-11-09 11:40:39 +0100

    Ajoute une méthode Instance.getCostFor()

--- src/main/scala/tsp/Instance.scala
+++ src/main/scala/tsp/Instance.scala
@@ -5,4 +5,19 @@ package tsp
   * @constructor create a new Instance.
   * @param costs cost of the several objectives of the instance
   */
-class Instance(val costs: List[Vector[Vector[Int]]])
+class Instance(val costs: List[Vector[Vector[Int]]]) {
+
+  /** Give the cost between two elements for a given objective.
+    *
+    * @param obj objective
+    * @param i first element
+    * @param j second element
+    * @return cost between i and j for the obj objective
+    */
+  def getCostFor(obj: Int, i: Int, j: Int): Int =
+    if (i <= j)
+      this.costs(obj)(i)(j)
+    else
+      this.costs(obj)(j)(i)
+
+}
