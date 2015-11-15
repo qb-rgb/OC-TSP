d79127a126886451068db956d4528ba6f69fcf5f
qw0te <baert.quentin@gmail.com>
2015-11-15 12:53:20 +0100

    Ajoute les différentes instances aux package tsp

--- src/main/scala/tsp/tsp.scala
+++ src/main/scala/tsp/tsp.scala
@@ -1,2 +1,19 @@
 /** Contain all the classes to represent TSP problem elements. */
-package object tsp
+package object tsp {
+
+  /** Instance with objectives A and B. */
+  val randomAB100 = InstanceBuilder.buildFrom(
+    List("data/randomA100.tsp", "data/randomB100.tsp")
+  )
+
+  /** Instance with objectives C and D. */
+  val randomCD100 = InstanceBuilder.buildFrom(
+    List("data/randomC100.tsp", "data/randomD100.tsp")
+  )
+
+  /** Instance with objectives E and F. */
+  val randomEF100 = InstanceBuilder.buildFrom(
+    List("data/randomE100.tsp", "data/randomF100.tsp")
+  )
+
+}
