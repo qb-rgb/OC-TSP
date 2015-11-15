703129162febeaab2dc559bf22b15aa019c18cdd
qw0te <baert.quentin@gmail.com>
2015-11-15 13:24:10 +0100

    Ajoute un trait Filter pour représenter les filtres de solutions TSP

---
+++ src/main/scala/filters/Filter.scala
@@ -0,0 +1,15 @@
+package filters
+
+import tsp.Solution
+
+/** Represent a filter of TSP solution. */
+trait Filter {
+
+  /** Filter a set of TSP solutions and yield thhe non-dominated ones.
+    *
+    * @param solutions set of solutions to filter
+    * @return set of non-dominated solutions
+    */
+  def filter(solutions: Set[Solution]): Set[Solution]
+
+}
