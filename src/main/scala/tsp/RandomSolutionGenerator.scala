a9dd56cd008a4175129192bea8eff11ae54c0fc3
qw0te <baert.quentin@gmail.com>
2015-11-15 13:12:04 +0100

    Ajoute une objet RandomSolutionGenerator qui génère des solutions de TSP aléatoire

---
+++ src/main/scala/tsp/RandomSolutionGenerator.scala
@@ -0,0 +1,16 @@
+package tsp
+
+/** Generate random TSP solutions. */
+object RandomSolutionGenerator {
+
+  import scala.util.Random
+
+  /** Generate a random TSP solution.
+    *
+    * @param length length of the solution to Generate
+    * @return random solution with the given length
+    */
+  def generateRandomSolution(length: Int): Solution =
+    Random shuffle (0 until length).toList
+
+}
