942315ac6e0d76fdbe666a962b9465b407a8a8a2
qw0te <baert.quentin@gmail.com>
2015-11-16 11:45:51 +0100

    Ajoute de la documentation

--- src/main/scala/tsp/tsp.scala
+++ src/main/scala/tsp/tsp.scala
@@ -19,6 +19,12 @@ package object tsp {
     List("data/randomE100.tsp", "data/randomF100.tsp")
   )
 
+  /** Determine if a solution domine another.
+    *
+    * @param s1Cost costs of the first solution
+    * @param s2Cost costs of the second solution
+    * @return true if the first solution domine the second, false otherwise
+    */
   def domine(s1Cost: Vector[Int], s2Cost: Vector[Int]): Boolean =
     (s1Cost zip s2Cost) forall {
       case (s1Cost, s2Cost) => s1Cost <= s2Cost
