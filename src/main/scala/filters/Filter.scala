6390849063082cfa7a5f3bd07a6c53272d4ff5ae
qw0te <baert.quentin@gmail.com>
2015-11-16 11:45:14 +0100

    Ajoute une méthode Filter.exportToGnuplotWith() qui permet de créer un fichier gnuplot à partir des résultats d'un filtre

--- src/main/scala/filters/Filter.scala
+++ src/main/scala/filters/Filter.scala
@@ -8,11 +8,43 @@ import tsp.{Solution, Instance}
   */
 abstract class Filter(val instance: Instance) {
 
+  import java.io.{File, BufferedWriter, FileWriter}
+
   /** Filter a set of TSP solutions and yield the non-dominated ones.
     *
     * @param solutions set of solutions to filter
     * @return set of non-dominated solutions
     */
   def filter(solutions: Set[Solution]): Set[Solution]
+
+  def exportToGnuplotWith(solutions: Set[Solution]): Unit = {
+    val nonDominatedSolutions = this filter solutions
+    val dominatedSolutions = solutions -- nonDominatedSolutions
+
+    def processSolution(sCosts: Vector[Int]): String =
+      sCosts mkString "\t"
+
+    // non-dominated.dat
+
+    val nonDominatedFile = new File("non-dominated.dat")
+    val bw1 = new BufferedWriter(new FileWriter(nonDominatedFile))
+    val nonDominatedText = nonDominatedSolutions map {
+      solution => processSolution(this.instance solutionCosts solution)
+    }
+
+    bw1 write (nonDominatedText mkString "\n")
+    bw1.close
+
+    // dominated.dat
+
+    val dominatedFile = new File("non-dominated.dat")
+    val bw2 = new BufferedWriter(new FileWriter(dominatedFile))
+    val dominatedText = dominatedSolutions map {
+      solution => processSolution(this.instance solutionCosts solution)
+    }
+
+    bw2 write (dominatedText mkString "\n")
+    bw2.close
+  }
 
 }
